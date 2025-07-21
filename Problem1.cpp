0c200a423124d1255e44418bd6a79d2b6161bbbd
황정빈 <hjbin1211@gmail.com>
2025-07-21 20:18:22 +0900

    dust map

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
 class City {
 private:
+    // 도시 전역
     char cityGround[100][100];
+    // 점수
     int score = 100;
+    int coalPower = 300; // 단위는 MW
+    float dustMap[100][100] = {20}; // 도시 전역의 미세먼지 지수 단위는 PM 2.5
 
+    // 도시 전역 출력
     void getCityGround() {
         for (int i = 0; i < 100; ++i) {
             for (int j = 0; j < 100; ++j)
@@ -14,8 +21,93 @@ private:
         }
     }
 
+    // 도시 전역의 미세먼지 농도 출력
+    void getCityDust() {
+        for (int i = 0; i < 100; ++i) {
+            for (int j = 0; j < 100; ++j)
+                cout << dustMap[i][j] << " ";
+            cout << endl;
+        }
+    }
+
+    // 각 건물별 미세먼지 영향량
+    int getEmission(char c) {
+        switch(c) {
+            case 'C': return 40;   // 석탄발전소
+            case 'F': return 7;    // 자동차 공장
+            case 'A': return 1;    // 행정시설
+            case 'H': return 0;    // 병원
+            case 'T': return -10;  // 유원지(완화)
+            case 'M': return -5;   // 목초지(완화)
+            case 'E': return 2;    // 기타
+            default: return 0;
+        }
+    }
+
+    // 거리
+    double distance(int x1, int y1, int x2, int y2) {
+        return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    // 미세먼지 농도 초기화
+    void initDustMap() {
+        for (int i = 0; i < 100; ++i) {
+            for (int j = 0; j < 100; ++j) {
+                dustMap[i][j] = 20;
+                dustMap[i][j] += getEmission(cityGround[i][j]);
+                if (dustMap[i][j] < 0) dustMap[i][j] = 0;
+            }
+        }
+        
+        for (int i = 0; i < 100; ++i) {
+            for (int j = 0; j < 100; ++j) {
+                int maxEmission = 0;
+                for (int x = max(0, i - 5); x <= min(99, i + 5); ++x) {
+                    for (int y = max(0, j - 5); y <= min(99, j + 5); ++y) {
+                        int emission = getEmission(cityGround[x][y]);
+                        if (emission > 0) {
+                            double dist = distance(i, j, x, y);
+                            if (dist <= 5) {
+                                // 거리 가중치 적용 (가까울수록 영향 큼)
+                                int weightedEmission = (int)(emission * (5 - dist) / 5);
+                                if (weightedEmission > maxEmission) {
+                                    maxEmission = weightedEmission;
+                                }
+                            }
+                        }
+                    }
+                }
+                dustMap[i][j] += maxEmission;
+            }
+        }
+
+        for (int i = 0; i < 100; ++i) {
+            for (int j = 0; j < 100; ++j) {
+                int maxReduction = 0;
+                for (int x = max(0, i - 5); x <= min(99, i + 5); ++x) {
+                    for (int y = max(0, j - 5); y <= min(99, j + 5); ++y) {
+                        int emission = getEmission(cityGround[x][y]);
+                        if (emission < 0) {
+                            double dist = distance(i, j, x, y);
+                            if (dist <= 5) {
+                                int weightedReduction = (int)(-emission * (5 - dist) / 5);
+                                if (weightedReduction > maxReduction) {
+                                    maxReduction = weightedReduction;
+                                }
+                            }
+                        }
+                    }
+                }
+                dustMap[i][j] -= maxReduction;
+                if (dustMap[i][j] < 0) dustMap[i][j] = 0;
+            }
+        }
+    }
+
 public:
-    City() {
+    // 생성자
+    City(int coalPower) : coalPower(coalPower) {
+        // 도시 전역의 건물 초기화
         for (int i = 0; i < 100; ++i) {
             for (int j = 0; j < 100; ++j)
                 cityGround[i][j] = 'E';
@@ -35,15 +127,28 @@ public:
             for (int j = 88; j <= 99; ++j)
                 cityGround[i][j] = 'C';
         }
+        for (int i = 83; i <= 87; ++i) {
+            for (int j = 83; j <= 87; ++j) {
+                cityGround[i][j] = 'M';
+            }
+        }
+
+        // 도시 전역의 미세먼지 지수 초기화
+        initDustMap();
     }
 
+    // 메인 호출 부분
     void usingCity() {
-        getCityGround();
+        getCityDust();
     }
 
 };
 
 int main() {
-    City callingVar;
+    int coalPower;
+    cout << "발전소 활동률(100, 70, 30): ";
+    cin >> coalPower;
+
+    City callingVar = City(300 * coalPower / 100);
     callingVar.usingCity();
 }
